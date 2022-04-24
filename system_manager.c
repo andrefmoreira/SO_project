4ae2a0d9ee7d787a2f9ca7b9d2a8d09e2912ac2e
André Moreira <82107406+andrefmoreira@users.noreply.github.com>
2022-04-24 12:39:50 +0100

    Update system_manager.c

--- system_manager.c
+++ system_manager.c
@@ -15,6 +15,18 @@ Pedro Miguel Pereira Catorze Nº 2020222916
 #include <string.h>
 #include <semaphore.h>
 
+typedef struct{
+    int id;
+    int n_instrucoes;
+    int tempo_maximo;
+    int prioridade;
+}task;
+
+
+//task manager: criar uma fila que ira ter tamanho QUEUE_POS , se a fila ja estiver cheia apagamos a task recevida e escrevemos no log.
+//thread scheduler vai ver as tarefas que estao na fila e dar-lhes prioridades. Se chegar um pedido novo as prioridades sao reavaliadas. Se o prazo maximo de execucao de uma tarefa ja tiver passado eliminamos essa tarefa.
+//as prioridades sao baseadas no tempo maximo de execucao e , o tempo de chegada a fila.
+
 
 typedef struct {
     int capac_proc1;
@@ -37,11 +49,18 @@ char s[64];
 char queue_pos[20];
 char max_wait[20];
 char edge_server_name[64];
+struct task *num_tasks;
 
 FILE *log_file;
 FILE *config_file;
 
+
 void *vcpu(void *u) {
+
+    //determinar o tempo que demora, se for menor que o tempo maximo da task removemos a task.
+    //realizar a task que tem prioridade 1
+
+
     pthread_mutex_lock(&mutex);
 
     //codigo do vcpu
@@ -123,17 +142,23 @@ void thread_scheduler(){
 
 void edge_server() {
 
-    //long capacity1 , capacity2;
+    int capacity1 , capacity2;
     char server_name[64];
 
-    //capacity1 = my_sharedm->capac_proc1;
-    //capacity2 = my_sharedm->capac_proc2;
+    capacity1 = my_sharedm->capac_proc1;
+    capacity2 = my_sharedm->capac_proc2;
 
     strcpy(server_name ,edge_server_name);
 
     pthread_t thread_vcpu[2];
     int id[2];
 
+
+    //se estiver num nivel de so utilizar um , tem o nivel de so usar o segundo ou so usar o primeiro.
+    id[i] = i;
+    pthread_create(&thread_vcpu[i], NULL, vcpu, (void *) &id[i]);
+
+    //se for um dos niveis ativa mos os dois.
     for (int i = 0; i < 2; i++) {
         id[i] = i;
         pthread_create(&thread_vcpu[i], NULL, vcpu, (void *) &id[i]);
@@ -147,6 +172,44 @@ void edge_server() {
 
 
 
+void reavaliar_prioridade(){
+
+    int prioridade = 1;
+    int length = sizeof(num_tasks) / sizeof(task);
+
+    for(int i = 0 ; i < length ; i++){
+
+        prioridade = 1;
+
+        for(int x = 0 ; x < length ; i++){
+            
+            if(num_tasks[i].tempo_maximo < num_tasks[x].tempo_maximo)
+                prioridade++; 
+            else if(num_tasks[i].tempo_maximo == num_tasks[x].tempo_maximo){
+                //tem o mesmo tempo maximo mas a task atual foi inserida depois da task a que esta a comparar.
+                if(i > x)
+                    prioridade++;
+
+            }
+        }
+
+        num_tasks[i].prioridade = prioridade;
+    }
+
+}
+
+void apagar_task(int indice ){
+
+    int length = sizeof(num_tasks) / sizeof(task);
+
+    for (i = indice - 1; i < length -1; i++)  
+    {  
+        num_tasks[i] = num_tasks[i+1]; // assign arr[i+1] to arr[i]  
+    }  
+    
+}
+
+
 void task_manager() {
 
     int x = 0;
@@ -155,6 +218,8 @@ void task_manager() {
     char *tokens;
 
     edge_servers = read_file();
+
+    num_tasks = malloc(queue_pos * sizeof(task));
 
 
     for (int i = 0 ; i < edge_servers ; i++) {
