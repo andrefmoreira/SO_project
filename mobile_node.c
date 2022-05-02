3615475a6ddf4279db869e785ca4108d81d08dc4
Pedro 14 <103061924+pedro-14@users.noreply.github.com>
2022-05-02 17:00:16 +0100

    Update mobile_node.c

--- mobile_node.c
+++ mobile_node.c
@@ -8,7 +8,6 @@ Pedro Miguel Pereira Catorze Nº 2020222916
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
 
 #define PIPE_NAME "TASK_PIPE"
@@ -18,13 +17,14 @@ typedef struct task {
     int id;
     int num_instr;
     double temp_max;
-    int prioridade;
+    int priority;
     double time;
 } task;
 
 
 int main(int argc, char *argv[]) {
-    int num_ped, intervalo;
+
+    int num_requests, interval;
     task t;
 
     if (argc != 5) {
@@ -32,57 +32,55 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
-    if (atoi(argv[1]) != 0) {
-        num_ped = atoi(argv[1]);
-    } else {
+    if (atoi(argv[1]) == 0) {
         printf("Error on the first parameter!\n");
         exit(-1);
     }
+    num_requests = atoi(argv[1]);
 
-    if (atoi(argv[2]) != 0) {
-        intervalo = atoi(argv[2]);
-    } else {
+
+    if (atoi(argv[2]) == 0) {
         printf("Error on the second parameter!\n");
         exit(-1);
     }
+    interval = atoi(argv[2]);
 
-    if (atoi(argv[3]) != 0) {
-        t.num_instr = atoi(argv[3]);
-    } else {
+
+    if (atoi(argv[3]) == 0) {
         printf("Error on the third parameter!\n");
         exit(-1);
     }
+    t.num_instr = atoi(argv[3]);
 
 
-    if (atoi(argv[4]) != 0) {
-        t.temp_max = atof(argv[4]);
-    } else {
+    if (atoi(argv[4]) == 0) {
         printf("Error on the fourth parameter!\n");
+        exit(-1);
+    }
+    t.temp_max = atof(argv[4]);
+
+
+    int fd;
+    if ((fd = open(PIPE_NAME, O_WRONLY)) < 0) {
+        perror("Can't open pipe for writting!\n");
         exit(-1);
     }
 
 
-
-    int fd;
-
-    if ((fd = open(PIPE_NAME, O_WRONLY)) < 0) {
-        perror("Cannot open pipe for writing: ");
-        exit(0);
-    }
-
-
-    t.prioridade = 1;
+    t.priority = 1;
     t.time = 0;
 
 
-    int intervalo_micro = intervalo * 1000;
+    int interval_micro = interval * 1000;
 
-    for(int i = 0 ; i < num_ped ; i++){
-    
+    char *string = NULL;
+    sprintf(string, "%d %d %lf %d %lf", num_requests, interval, t.temp_max , t.priority, t.time);
+
+    for (int i = 0 ; i < num_requests ; i++) {
         t.id = i;
         write(fd, &t, sizeof(task));
 		printf("task %d enviada.\n",i);
-        usleep(intervalo_micro);
+        usleep(interval_micro);
     }
 
     return 0;
